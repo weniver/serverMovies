0fda4fdea42c3e55e3473a1323c698435552ad24
weniver <>
2021-02-01 12:19:16 -0600

    Add movie route with error handling

--- routes/api.js
+++ routes/api.js
@@ -10,15 +10,14 @@ router.post("/add/movie", async (req, res) => {
   try {
     const movie = new Movie(req.body);
     const savedMovie = await movie.save();
-    res.status(200).send("vengo del server signup");
+    res.status(200).send(savedMovie);
   } catch (e) {
-    console.log(e);
-    res.send("error");
+    res.status(500).json({ message: e.toString() });
   }
 });
 router.get("/movies", async (req, res) => {
   try {
-    const allMovies = await Movie.findee();
+    const allMovies = await Movie.find();
     res.json(allMovies);
   } catch (e) {
     res.status(500).json({ message: e.toString() });
