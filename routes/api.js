3596d5e71f368837f7bc23ac5cc366b106ccc328
weniver <>
2021-02-12 16:45:09 -0600

    Edit movie

--- routes/api.js
+++ routes/api.js
@@ -53,4 +53,16 @@ router.delete("/movie/:id", async (req, res) => {
   }
 });
 
+router.patch("/movie/:id", async (req, res) => {
+  try {
+    let updatedMovie = await Movie.findOneAndUpdate(
+      { _id: req.params.id },
+      req.body
+    );
+    res.status(200).json(updatedMovie);
+  } catch (e) {
+    res.status(500).json({ message: e.toString() });
+  }
+});
+
 module.exports = router;
